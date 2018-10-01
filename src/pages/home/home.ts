e0ceb624cab9924e2a17f8179de9a894875cabd4
alumno Secundaria <al3201@cedi.local>
2018-10-01 08:42:07 -0500

    Nuevo branch pye

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { HistoriaPage } from '../historia/historia';
+import { DibujoPage } from '../dibujo/dibujo';
 
 @Component({
   selector: 'page-home',
@@ -8,6 +9,7 @@ import { HistoriaPage } from '../historia/historia';
 })
 export class HomePage {
   historia = HistoriaPage;
+  dibujo = DibujoPage;
 
   constructor(public navCtrl: NavController) {
 
@@ -15,5 +17,9 @@ export class HomePage {
   clickHistoria(){
     this.navCtrl.push(this.historia);
   }
+  clickDibujo(){
+    this.navCtrl.push(this.dibujo);
+  }
+
 
 }
